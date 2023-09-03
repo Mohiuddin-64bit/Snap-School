2882115d0295c92d07673031c5864066373c5072
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-09-03 18:04:44 +0600

    Instructor card updated

--- src/Pages/Instructor/Instructor.jsx
+++ src/Pages/Instructor/Instructor.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import { BsFillPersonFill } from "react-icons/Bs";
+import { MdOutlineAlternateEmail } from "react-icons/Md";
+import { CiMonitor } from "react-icons/Ci";
 
 const Instructor = () => {
-  const [instructor, setInstructor] = useState([])
+  const [instructor, setInstructor] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:5000/allInstructor")
@@ -16,18 +18,31 @@ const Instructor = () => {
       });
   }, []);
   return (
-<div className="container mx-auto mt-12">
+    <div className="container mx-auto mt-12 grid grid-cols-2 gap-12">
       {instructor.map((cl) => (
-        <div key={cl._id}>
-          <div className="card flex items-center gap-3 mb-12 lg:card-side bg-base-100 shadow-xl">
-            <figure className="w-32">
-              <img src={cl.photo} alt="Album" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{cl.name}</h2>
-              <h2 className="text-lg text-gray-500">{cl.email}</h2>
-              <p className="font-semibold">Class Taken: {cl.classesTaken}</p>
-            </div>  
+        <div key={cl._id} className="">
+          <div className="flex items-center gap-3 mb-12 rounded-lg shadow-xl">
+            <img
+              src={cl.photo}
+              className="w-[200px] rounded-l-lg h-[150px] object-cover"
+              alt="photos"
+            />
+            <div>
+              <div className="flex items-center gap-1">
+                <BsFillPersonFill></BsFillPersonFill>
+                <h2 className="text">{cl.name}</h2>
+              </div>
+              <div className="flex items-center gap-1">
+                <MdOutlineAlternateEmail></MdOutlineAlternateEmail>
+                <h2 className="text-lg text">{cl.email}</h2>
+              </div>
+              <div className="flex items-center gap-1">
+                <CiMonitor></CiMonitor>
+                <p className="font-semibold text">
+                  Class Taken: {cl.classesTaken}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       ))}
